456a09c08456171fd22c61da081f58d3488c5c37
高木 萌子 <barbiepinkmindoll@gmail.com>
2015-06-04 01:22:51 +0900

    入力された値全て辞書にある場合

--- Game16.java
+++ Game16.java
@@ -1,11 +1,11 @@
 //与えられた16個の文字を使いなるべく長い単語を作る
-//*/user/share/dict/wordsを辞書として使う
+//*/usr/share/dict/wordsを辞書として使う
 
 
 import java.util.*;
 import java.io.*;
 public class Game16{
-	public static void main(String[] args){
+	public static void main(String[] args) throws IOException{
 		//入力
 		System.out.println("16文字のアルファベットを入力してください。");
 		Scanner sc = new Scanner(System.in);
@@ -15,36 +15,42 @@ public class Game16{
 
 		//辞書
 		char[][] dictionary = new char[300000][16];
+		char[][] normalDictionary = new char[300000][16];
 		int i = 0;
 		int j = 0;
-		try{
-			BufferedReader br = new BufferedReader(new FileReader("/usr/share/dict/words"));
-			String line = "";
-			while((line = br.readLine()) != null){
-				line = line.toLowerCase();
-				dictionary[i] = sort(line);
-				if(dictionary[i].length >= words.length){
-					for(j = 0; j < words.length; j++){
-						if(Arrays.equals(dictionary[i], words) == true){
-							System.out.println(dictionary[i]);
-						}
-					}
-				}else{
-					for(j = 0; j < dictionary[i].length; j++){
-						if(Arrays.equals(dictionary[i], words) == true){
-							System.out.println(dictionary[i]);
-						}
+		int k = 0;
+		BufferedReader br = new BufferedReader(new FileReader("/usr/share/dict/words"));//辞書から読み込み
+		String line = "";
+		//辞書と比較して、その言葉の添字を返す
+		int number = 0;
+		while((line = br.readLine()) != null){
+			line = line.toLowerCase();
+			normalDictionary[k] = line.toCharArray();//普通の辞書を配列に格納
+			dictionary[i] = sort(line);
+			if(dictionary[i].length >= words.length){
+				for(j = 0; j < words.length; j++){
+					if(Arrays.equals(dictionary[i], words) == true){
+						number = i;			
 					}
 				}
-				i++;
+			}else{
+				for(j = 0; j < dictionary[i].length; j++){
+					if(Arrays.equals(dictionary[i], words) == true){
+						number = i;
+					}
+				}
 			}
-			br.close();
-		}catch(FileNotFoundException e){
-			System.out.println(e);
-		}catch(IOException e){
-			System.out.println(e);
+			i++;
+			k++;
 		}
+		br.close();
+		//numberには、辞書の該当する添字が入っている
+		System.out.println(number);
+		//普通の辞書でまた探索して値を返す。
+		System.out.println(dictionary[number]);
+		System.out.println(normalDictionary[number]);
 	}
+	
 	//Sort
 	static char[] sort(String str){
 		char[] a = new char[str.length()];
